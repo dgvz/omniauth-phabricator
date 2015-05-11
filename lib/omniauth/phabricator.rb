36cce4e041404397e1304f52cf0dca1894393c55
Dylan Griffith <dyl.griffith@gmail.com>
2015-05-11 17:43:44 +1000

    implement phabricator omniauth strategy

--- lib/omniauth/phabricator.rb
+++ lib/omniauth/phabricator.rb
@@ -2,6 +2,6 @@ require "omniauth/phabricator/version"
 
 module Omniauth
   module Phabricator
-    # Your code goes here...
   end
 end
+require "omniauth/strategies/phabricator"
---
+++ lib/omniauth/strategies/phabricator.rb
@@ -0,0 +1,36 @@
+require 'omniauth-oauth2'
+
+module OmniAuth
+  module Strategies
+    class Phabricator < OmniAuth::Strategies::OAuth2
+      option :client_options, {
+        :token_url => "/oauthserver/token/"
+      }
+
+      def request_phase
+        options[:scope] ||= 'whoami'
+        options[:response_type] ||= 'code'
+        super
+      end
+
+      uid{ raw_info['phid'] }
+
+      info do
+        {
+          :name => raw_info['realName'],
+          :email => raw_info['primaryEmail']
+        }
+      end
+
+      extra do
+        {
+          'raw_info' => raw_info
+        }
+      end
+
+      def raw_info
+        @raw_info ||= access_token.get("/api/user.whoami?access_token=#{access_token.token}").parsed
+      end
+    end
+  end
+end

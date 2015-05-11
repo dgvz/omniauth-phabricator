92672fa16572eb8de9ea577f8b3ed7caa1b11277
Dylan Griffith <dyl.griffith@gmail.com>
2015-05-11 15:12:43 +1000

    initial gem

---
+++ lib/omniauth/phabricator.rb
@@ -0,0 +1,7 @@
+require "omniauth/phabricator/version"
+
+module Omniauth
+  module Phabricator
+    # Your code goes here...
+  end
+end
---
+++ lib/omniauth/phabricator/version.rb
@@ -0,0 +1,5 @@
+module Omniauth
+  module Phabricator
+    VERSION = "0.1.0"
+  end
+end
